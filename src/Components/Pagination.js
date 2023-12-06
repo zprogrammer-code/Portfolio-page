9a0e5672e8f4b31cbe17b18fab2be526a605514b
Zachary Jacobs <zprogrammercode@gmail.com>
2023-12-05 23:27:42 -0700

    saving, in the middle of error fixing

---
+++ src/Components/Pagination.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { Button } from 'react-bootstrap';
+
+export function PageNavigation() {
+    return(<>
+<Nav aria-label="Page navigation" className="flex-center pagination-container">
+    <ul class="pagination">
+    <Button variant='light'>
+        <a href="Store" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+        </a>
+    </Button>
+    <Button variant='light'><Nav.Link href="Home">1</Nav.Link></Button>
+    <Button variant='light'><Nav.Link href="Store">2</Nav.Link></Button>
+    <Button variant='light'><Nav.Link href="Store">3</Nav.Link></Button>
+    <Button variant='light'><Nav.Link href="Donate">4</Nav.Link></Button>
+    <Button variant='light'><Nav.Link href="Contact">5</Nav.Link></Button>
+    <Button variant='light'>
+        <a href="Checkout" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        </a>
+    </Button>
+    </ul>
+</Nav>
+    </>);
+};
---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '/home/zprogrammercode/React-portfolio-page/portfolio-page1/src/Components/Components.css';
+import HeroSection from '../Components/HeroSection';
+
+function HomePage(){
+
+   return( 
+      <> 
+      <div className='HomePageContainer'>
+     <HeroSection/>
+      </div>
+     </>)
+      };
+
+export default HomePage;
+
