a6904d400b3c0986097730a6b18e64ebc2509470
Zachary Jacobs <zprogrammercode@gmail.com>
2023-12-12 20:22:47 -0700

    I am know uploading updates and an alert bar connecting to one of my current projects

--- src/Components/AlertBar.js
+++ src/Components/AlertBar.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert'; 
-import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "/home/zprogrammercode/React-portfolio-page/portfolio-page1/src/Components/Components.css";
 
  
 function AlertBar() {
     return(
         <>
-            <Alert varient="dark" href="#" className=" alert block-example border border-0 border-dark" >
+            <Alert varient="success" className=" alert" >
                 <Container className='anouncement-content-wrapper'> 
-                   <h4 className='alert-content-padding'> See what im working on </h4>
-                    <Button type="pill" >
-                        My Github
-                    </Button>
+                   <h2 className='alert-content-padding'> See what im working on </h2>
+                    <button variant='dark' >
+                       <a href="https://github.com/zprogrammer-code/ecommerce-ultra"> My Github </a>
+                    </button>
                 </Container>
             </Alert>
         </>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '/home/zprogrammercode/React-portfolio-page/portfolio-page1/src/Components/Components.css';
 import HeroSection from '../Components/HeroSection';
 import PortfolioNavbar from '../Components/Navbar';
+import AlertBar from '../Components/AlertBar';
 
 const HomePage = () => {
 
    return( 
       <> 
       <div className='HomePageContainer'>
+         <AlertBar/>
          <PortfolioNavbar/>
          <HeroSection/>
          </div>
