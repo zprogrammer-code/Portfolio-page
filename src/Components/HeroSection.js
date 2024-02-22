6dd2a766fbb9041d06929766e57f0bfb61a3e711
Zachary Jacobs <zprogrammercode@gmail.com>
2024-02-21 21:24:55 -0700

    some quick literature errors

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -14,9 +14,9 @@ function HeroSection() {
             <iframe src="https://drive.google.com/file/d/1WFCAQmSPbIy10GzVNSBS-3MGq8tY-CnW/preview" width="450" height="480" allow="autoplay" title='myFace'></iframe>              
              <div className='info'>
               <h1 className='namefont'> Hi, My name is Zachary Jacobs</h1>
-                    <p className='summary'>Im a fullstack software dveloper. My specialties
-                     are <s className='txtdec'>Javascript/React.js & React Native,
-                    Ruby/Rails and mySQL.</s> Java, Ruby and Javascript
+                    <p className='summary'>Im a fullstack software developer and digital marketer. My specialties
+                     are <s className='txtdec'>Javascript/React.js & React Native, Redux, Sass/CSS,
+                    Ruby/Rails and node/mySQL.</s> Java, Ruby and Javascript
                     are the object oriented languages that I know</p>
                     <p> I Transitioned into Software Engineering as an entrepreneur
                          to use old-world craftsmanship skills to problem solve and build modern solutions in the tech industry. Avid fitness buff.
@@ -24,7 +24,7 @@ function HeroSection() {
                     <h3>7193384333</h3>
                     <h3>zprogrammercode@gmail.com</h3>
                     <h3>Colorado Springs, CO 80909</h3>
-                    <h2>Click on the M to send me an email
+                    <h2>Click on the <a>M</a> to send me an email
                     </h2>
             </div>
             </div>
